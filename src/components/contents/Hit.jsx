4bca263aebc52354700592f0d880b18401da0393
kmj <designshake74@gmail.com>
2024-06-09 18:18:38 +0900

    0609-gs쿠폰 Part1.메인작업중

--- src/components/contents/Hit.jsx
+++ src/components/contents/Hit.jsx
@@ -2,7 +2,84 @@ import React from 'react'
 
 const Hit = () => {
   return (
-    <div>Hit</div>
+    <section id='hit'>
+      <h2 className='title'>지금 가장 많이 구매해요!</h2>
+      <div className='hit__inner'>
+        <div className='hit__thumb'>사진이미지</div>
+        <div className='hit__title'>[스타벅스]아이스 카페 아메리카노 Tall</div>
+        <div className='hit__text'>
+          <span className='price'>5,600원</span>
+          <span className='fixprice'>3,500원</span>
+          <span className='discount'>[-13%]</span>
+        </div>
+      </div>
+
+      <div className='hit__inner'>
+        <div className='hit__thumb'>사진이미지</div>
+        <div className='hit__title'>[스타벅스]아이스 카페 아메리카노 Tall</div>
+        <div className='hit__text'>
+          <span className='price'>5,600원</span>
+          <span className='fixprice'>3,500원</span>
+          <span className='discount'>[-13%]</span>
+        </div>
+      </div>
+
+      <div className='hit__inner'>
+        <div className='hit__thumb'>사진이미지</div>
+        <div className='hit__title'>[스타벅스]아이스 카페 아메리카노 Tall</div>
+        <div className='hit__text'>
+          <span className='price'>5,600원</span>
+          <span className='fixprice'>3,500원</span>
+          <span className='discount'>[-13%]</span>
+        </div>
+      </div>
+
+      <div className='hit__inner'>
+        <div className='hit__thumb'>사진이미지</div>
+        <div className='hit__title'>[스타벅스]아이스 카페 아메리카노 Tall</div>
+        <div className='hit__text'>
+          <span className='price'>5,600원</span>
+          <span className='fixprice'>3,500원</span>
+          <span className='discount'>[-13%]</span>
+        </div>
+      </div>
+      <div className='hit__inner'>
+        <div className='hit__thumb'>사진이미지</div>
+        <div className='hit__title'>[스타벅스]아이스 카페 아메리카노 Tall</div>
+        <div className='hit__text'>
+          <span className='price'>5,600원</span>
+          <span className='fixprice'>3,500원</span>
+          <span className='discount'>[-13%]</span>
+        </div>
+      </div>
+      <div className='hit__inner'>
+        <div className='hit__thumb'>사진이미지</div>
+        <div className='hit__title'>[스타벅스]아이스 카페 아메리카노 Tall</div>
+        <div className='hit__text'>
+          <span className='price'>5,600원</span>
+          <span className='fixprice'>3,500원</span>
+          <span className='discount'>[-13%]</span>
+        </div>
+      </div>
+      <div className='hit__inner'>
+        <div className='hit__thumb'>사진이미지</div>
+        <div className='hit__title'>[스타벅스]아이스 카페 아메리카노 Tall</div>
+        <div className='hit__text'>
+          <span className='price'>5,600원</span>
+          <span className='fixprice'>3,500원</span>
+          <span className='discount'>[-13%]</span>
+        </div>
+      </div>
+      <div className='hit__inner'>
+        <div className='hit__thumb'>사진이미지</div>
+        <div className='hit__title'>[스타벅스]아이스 카페 아메리카노 Tall</div>
+        <div className='hit__text'>
+          <span className='price'>5,600원</span>
+          <span className='fixprice'>3,500원</span>
+          <span className='discount'>[-13%]</span>
+        </div>
+      </div>
+    </section>
   )
 }
 
